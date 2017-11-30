c5642bae09319d875f956b7fa549a19f94ebf304
maurosilva2012 <mauro.silva.skype@gmail.com>
2017-11-30 22:54:53 +0100

    Adicionado Tabela de Produro, Tela de Consulta Produto e Cadastro de Produto.

---
+++ Mercado/RetaguardaTelas/FrmCadastroProduto.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace RetaguardaTelas
+{
+    partial class FrmCadastroProduto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FrmCadastroProduto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(742, 480);
+            this.Name = "FrmCadastroProduto";
+            this.Text = "FrmCadastroProduto";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
